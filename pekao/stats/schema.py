2b9629d3d8aba3752516ed18db471a1606b0670e
pawisoon <pawi552@gmail.com>
2019-06-08 11:42:29 +0200

    Update schema.py

--- pekao/stats/schema.py
+++ pekao/stats/schema.py
@@ -1,11 +1,7 @@
 from graphene_django import DjangoObjectType
 import graphene
-<<<<<<< HEAD
-from stats.models import User, Terminal, Payment, Employer
-=======
 from stats.models import User, Terminal, Payment, Raport, Employer
 
->>>>>>> 56ad34371dfe2c5d3b50660c87b04efc25e9b52a
 
 class EmployerNode(DjangoObjectType):
     class Meta:
@@ -38,8 +34,15 @@ class StatsType(graphene.ObjectType):
     pass
 
 
-<<<<<<< HEAD
+
+class Mutation(graphene.ObjectType):
+    create_raport = CreateRaportMutation.Field()
+
+
 class Query(graphene.ObjectType):
+
+    raports = graphene.List(RaportNode)
+    statistics = graphene.Field(StatsType)
     payment = graphene.Field(PaymentNode, id=graphene.Int())
 
     def resolve_payments(self, **kwargs):
@@ -47,19 +50,9 @@ class Query(graphene.ObjectType):
             return Payment.objects.get(pk=kwargs['id'])
         return 0
 
-
-=======
-class Mutation(graphene.ObjectType):
-    create_raport = CreateRaportMutation.Field()
-
-
-class Query(graphene.ObjectType):
-    raports = graphene.List(RaportNode)
-    statistics = graphene.Field(StatsType)
-
     def resolve_raports(self, info):
         return Raport.objects.filter(employer__owner=info.context.user)
 
     def resolve_statistics(self, info):
         return None
->>>>>>> 56ad34371dfe2c5d3b50660c87b04efc25e9b52a
+
