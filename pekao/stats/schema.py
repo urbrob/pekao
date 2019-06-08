1aebea9cdd92b56fc4ca3fa2dea4bfc4d2dc719f
DawidCwiek <cwiekdawid1@gmail.com>
2019-06-08 11:58:04 +0200

    nie wiem co zmienilem

--- pekao/stats/schema.py
+++ pekao/stats/schema.py
@@ -16,10 +16,21 @@ class PaymentNode(DjangoObjectType):
     class Meta:
         model = Payment
 
+class CreateRaportMutation(graphene.Mutation):
+    class Arguments:
+        employer = graphene.Int()
 
+    status = graphene.String()
+
+     def mutate(self, info, **kwargs):
+        # generateraport(kwargs['employer'])
+        return CreateRaportMutation(status='OK')
 
 class Mutation(graphene.ObjectType):
     pass
 
 class Query(graphene.ObjectType):
-    pass
+
+     def resolve_customer_count(self, info, text=None):
+         return Payment.object.filer(terminal=Terminal.object.filter(employer=kwargs['employer_id'])).annotate(Count('card_namber', 
+                                         distinct=True))
