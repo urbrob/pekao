7ed6b006f42f93c761e2327e6530879e70e1e27f
DawidCwiek <cwiekdawid1@gmail.com>
2019-06-08 11:04:12 +0200

    add render pdf

--- pekao/stats/views.py
+++ pekao/stats/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.utils import timezone
+from .renderPDF import Render
 
-# Create your views here.
+
+class Pdf(View):
+
+    def get(self, request, id):
+        today = timezone.now()
+        params = {
+            'today': today,
+        }
+        return Render.render('pdf/pdf.html', params)
