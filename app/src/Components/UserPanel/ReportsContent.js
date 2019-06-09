be92f6923772b18d67f710eac15940f83b7f4b9a
Graleszczur <Grales76@onet.pl>
2019-06-09 03:24:01 +0200

    add view enabling user to generate report

--- app/src/Components/UserPanel/ReportsContent.js
+++ app/src/Components/UserPanel/ReportsContent.js
@@ -2,15 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
+import {Paper} from "@material-ui/core";
+import DatePicker from "./DatePicker"
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 
 const styles = theme => ({
     paper: {
-        marginTop: "2%",
-        marginLeft: "5%",
-        marginRight: "5%",
+        width: 500,
+        height: 400,
+        marginTop: "15%",
+        marginLeft: "70%",
         marginBottom: "5%",
+    },
+    genButton:{
+        color: "white",
+        width:200,
+        height:100,
+        marginTop: "10%",
+        marginLeft: "30%",
+        fontSize: 24,
+        backgroundColor: "red",
     },
 });
 
@@ -23,7 +36,15 @@ function ReportsContent(props) {
             <div className={classes.root}>
                 <Grid container direction="row">
                     <Grid item>
+                        <Paper className={classes.paper}>
 
+                            <Typography style={{paddingTop:"7%", marginBottom:"5%"}} align="center" variant="h4">
+                                Pick date of which you want to generate report of.</Typography>
+                            <DatePicker/>
+                            <Button className={classes.genButton}>
+                                Generate
+                            </Button>
+                        </Paper>
                     </Grid>
                 </Grid>
 
