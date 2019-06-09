176cf1d5845b88cc238cfee1e836da4641ca5c81
Robert Urbaniak <urbrob@MacBook-Pro-Robert.local>
2019-06-09 12:47:53 +0200

    Impact view

--- app/src/Components/UserPanel/ImpactContent.js
+++ app/src/Components/UserPanel/ImpactContent.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 
 const styles = theme => ({
@@ -20,10 +22,25 @@ function ImpactContent(props) {
         <React.Fragment>
 
             <div className={classes.root}>
-                <Grid container direction="row">
-                    <Grid item>
-
+                <Grid container direction="row" justify="center" alignItems="center">
+                  <Paper style={{marginTop: "2%", marginRight: "5%"}}>
+                    <Grid item style={{marginTop: "2%", width: "300px"}}>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}}>Card Business Impact</Typography>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}} color='primary'>14.34%</Typography>
                     </Grid>
+                  </Paper>
+                  <Paper style={{marginTop: "2%", marginRight: "5%"}}>
+                    <Grid item style={{marginTop: "2%", width: "300px"}}>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}}>Local Business Rank</Typography>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}} color='primary'>4</Typography>
+                    </Grid>
+                  </Paper>
+                  <Paper style={{marginTop: "2%", marginRight: "5%"}}>
+                    <Grid item style={{marginTop: "2%", width: "300px"}}>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}}>Population Business Impact</Typography>
+                      <Typography align='center' variant='h4' style={{marginBottom: "15px"}} color='primary'>9.23%</Typography>
+                    </Grid>
+                  </Paper>
                 </Grid>
 
             </div>
