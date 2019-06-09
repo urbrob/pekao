502ebaa3aa1dcc145a62b77056c2e42848dcbf1b
Graleszczur <Grales76@onet.pl>
2019-06-09 15:20:06 +0200

    add heatmaps for views

--- app/src/Components/Home/FreeHeatMap.js
+++ app/src/Components/Home/FreeHeatMap.js
@@ -1,21 +1,380 @@
 import React from 'react';
-import Map, { GoogleApiWrapper } from 'google-maps-react';
+import clsx from 'clsx';
+import Select from 'react-select';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import NoSsr from '@material-ui/core/NoSsr';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import MenuItem from '@material-ui/core/MenuItem';
+import CancelIcon from '@material-ui/icons/Cancel';
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
+import PropTypes from 'prop-types';
+import {Grid} from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
 
-export class FreeMap extends React.Component {
-    render() {
-        return (
-            <Map
-                google={this.props.google}
-                zoom={14}
-                center={{
-                    lat: 37.774929,
-                    lng: -122.419416,
-                }}
-            />
-        );
-    }
+
+const suggestions = [
+    { label: 'Wrocław' },
+    { label: 'Kawiarnie' },
+    { label: 'Albania' },
+    { label: 'Algeria' },
+    { label: 'American Samoa' },
+    { label: 'Andorra' },
+    { label: 'Angola' },
+    { label: 'Anguilla' },
+    { label: 'Antarctica' },
+    { label: 'Antigua and Barbuda' },
+    { label: 'Argentina' },
+    { label: 'Armenia' },
+    { label: 'Aruba' },
+    { label: 'Australia' },
+    { label: 'Austria' },
+    { label: 'Azerbaijan' },
+    { label: 'Bahamas' },
+    { label: 'Bahrain' },
+    { label: 'Bangladesh' },
+    { label: 'Barbados' },
+    { label: 'Belarus' },
+    { label: 'Belgium' },
+    { label: 'Belize' },
+    { label: 'Benin' },
+    { label: 'Bermuda' },
+    { label: 'Bhutan' },
+    { label: 'Bolivia, Plurinational State of' },
+    { label: 'Bonaire, Sint Eustatius and Saba' },
+    { label: 'Bosnia and Herzegovina' },
+    { label: 'Botswana' },
+    { label: 'Bouvet Island' },
+    { label: 'Brazil' },
+    { label: 'British Indian Ocean Territory' },
+    { label: 'Brunei Darussalam' },
+].map(suggestion => ({
+    value: suggestion.label,
+    label: suggestion.label,
+}));
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        height: 250,
+        margin: "10%",
+    },
+    input: {
+        display: 'flex',
+        padding: 0,
+        height: 'auto',
+    },
+    image: {
+        backgroundImage: 'url(https://i.imgur.com/K6cKyb1.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    valueContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flex: 1,
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    chip: {
+        margin: theme.spacing(0.5, 0.25),
+    },
+    chipFocused: {
+        backgroundColor: emphasize(
+            theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+            0.08,
+        ),
+    },
+    noOptionsMessage: {
+        padding: theme.spacing(1, 2),
+    },
+    singleValue: {
+        fontSize: 16,
+    },
+    placeholder: {
+        position: 'absolute',
+        left: 2,
+        bottom: 6,
+        fontSize: 16,
+    },
+    paper: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: theme.spacing(1),
+        left: 0,
+        right: 0,
+    },
+    divider: {
+        height: theme.spacing(2),
+    },
+}));
+
+function NoOptionsMessage(props) {
+    return (
+        <Typography
+            color="textSecondary"
+            className={props.selectProps.classes.noOptionsMessage}
+            {...props.innerProps}
+        >
+            {props.children}
+        </Typography>
+    );
 }
 
-export default GoogleApiWrapper({
-    apiKey: "AIzaSyAglZr6-asq6ROsX8eXyEy7ElXlNgj1I1Q",
-})(FreeMap);
+NoOptionsMessage.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    selectProps: PropTypes.object.isRequired,
+};
+
+function inputComponent({ inputRef, ...props }) {
+    return <div ref={inputRef} {...props} />;
+}
+
+inputComponent.propTypes = {
+    inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+};
+
+function Control(props) {
+    const {
+        children,
+        innerProps,
+        innerRef,
+        selectProps: { classes, TextFieldProps },
+    } = props;
+
+    return (
+        <TextField
+            fullWidth
+            InputProps={{
+                inputComponent,
+                inputProps: {
+                    className: classes.input,
+                    ref: innerRef,
+                    children,
+                    ...innerProps,
+                },
+            }}
+            {...TextFieldProps}
+        />
+    );
+}
+
+Control.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    selectProps: PropTypes.object.isRequired,
+};
+
+function Option(props) {
+    return (
+        <MenuItem
+            ref={props.innerRef}
+            selected={props.isFocused}
+            component="div"
+            style={{
+                fontWeight: props.isSelected ? 500 : 400,
+            }}
+            {...props.innerProps}
+        >
+            {props.children}
+        </MenuItem>
+    );
+}
+
+Option.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+    isFocused: PropTypes.bool,
+    isSelected: PropTypes.bool,
+};
+
+function Placeholder(props) {
+    return (
+        <Typography
+            color="textSecondary"
+            className={props.selectProps.classes.placeholder}
+            {...props.innerProps}
+        >
+            {props.children}
+        </Typography>
+    );
+}
+
+Placeholder.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    selectProps: PropTypes.object.isRequired,
+};
+
+function SingleValue(props) {
+    return (
+        <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
+            {props.children}
+        </Typography>
+    );
+}
+
+SingleValue.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    selectProps: PropTypes.object.isRequired,
+};
+
+function ValueContainer(props) {
+    return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
+}
+
+ValueContainer.propTypes = {
+    children: PropTypes.node,
+    selectProps: PropTypes.object.isRequired,
+};
+
+function MultiValue(props) {
+    return (
+        <Chip
+            tabIndex={-1}
+            label={props.children}
+            className={clsx(props.selectProps.classes.chip, {
+                [props.selectProps.classes.chipFocused]: props.isFocused,
+            })}
+            onDelete={props.removeProps.onClick}
+            deleteIcon={<CancelIcon {...props.removeProps} />}
+        />
+    );
+}
+
+MultiValue.propTypes = {
+    children: PropTypes.node,
+    isFocused: PropTypes.bool,
+    removeProps: PropTypes.object.isRequired,
+    selectProps: PropTypes.object.isRequired,
+};
+
+function Menu(props) {
+    return (
+        <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
+            {props.children}
+        </Paper>
+    );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node,
+    innerProps: PropTypes.object,
+    selectProps: PropTypes.object,
+};
+
+const components = {
+    Control,
+    Menu,
+    MultiValue,
+    NoOptionsMessage,
+    Option,
+    Placeholder,
+    SingleValue,
+    ValueContainer,
+};
+
+function IntegrationReactSelect() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [single, setSingle] = React.useState(null);
+    const [multi, setMulti] = React.useState(null);
+
+    function handleChangeSingle(value) {
+        setSingle(value);
+    }
+
+    function handleChangeMulti(value) {
+        setMulti(value);
+    }
+
+    const selectStyles = {
+        input: base => ({
+            ...base,
+            color: theme.palette.text.primary,
+            '& input': {
+                font: 'inherit',
+            },
+        }),
+    };
+
+    return (
+        <React.Fragment>
+            <Typography style={{margin: "10%"}} variant="h3" align="center">
+                Check your area potential
+            </Typography>
+            <div className={classes.root}>
+
+                <NoSsr>
+                    <Select
+                        classes={classes}
+                        styles={selectStyles}
+                        inputId="react-select-single"
+                        TextFieldProps={{
+                            label: 'Location',
+                            InputLabelProps: {
+                                htmlFor: 'react-select-single',
+                                shrink: true,
+                            },
+                            placeholder: 'Search a country (start with a)',
+                        }}
+                        options={suggestions}
+                        components={components}
+                        value={single}
+                        onChange={handleChangeSingle}
+                    />
+                    <div className={classes.divider} />
+                    <Select
+                        classes={classes}
+                        styles={selectStyles}
+                        inputId="react-select-multiple"
+                        TextFieldProps={{
+                            label: 'Category',
+                            InputLabelProps: {
+                                htmlFor: 'react-select-multiple',
+                                shrink: true,
+                            },
+                            placeholder: 'Select multiple countries',
+                        }}
+                        options={suggestions}
+                        components={components}
+                        value={multi}
+                        onChange={handleChangeMulti}
+                        isMulti
+                    />
+                </NoSsr>
+                <Grid container style={{marginTop:"30%"}}>
+                    <Grid item>
+                        <Card className={classes.card}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    alt="Contemplative Reptile"
+                                    height="400"
+                                    image="https://i.imgur.com/K6cKyb1.png"
+                                    title="Your map"
+                                />
+                            </CardActionArea>
+
+                        </Card>
+                    </Grid>
+
+
+                </Grid>
+            </div>
+        </React.Fragment>
+
+    );
+}
+
+export default IntegrationReactSelect;
